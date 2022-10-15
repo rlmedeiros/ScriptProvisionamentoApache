1273f41308827e9c7586fd03cada4f899bc2f005
rlmedeiros <13944920+rlmedeiros@users.noreply.github.com>
2022-10-15 00:51:19 -0300

    Create scriptApache.sh

---
+++ scriptApache.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Atualizando e instalando dependências..."
+apt-get update 
+apt-get upgrade -y
+apt-get install apache2 unzip -y
+
+echo "Fazendo download..."
+cd /tmp
+wget http://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando..." 
+unzip main.zip
+cd linux-site-dio-main
+
+cp -R * /var/www/html/
